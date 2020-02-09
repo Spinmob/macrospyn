100e320eab3b0edb7cd8d9a4963fae3680d0ebad
Jack Sankey <jack.sankey@gmail.com>
2020-02-09 17:47:09 -0500

    Lots of bug fixes
    Almost all of them involving garbage collection. Code is finally "stable" but needs a LOT of testing and interface work.

--- interface.py
+++ interface.py
@@ -11,6 +11,35 @@ else:                      _path_dll = _os.path.join(_os.path.split(__file__)[0]
 # Get the engine.
 _engine = _c.CDLL(_path_dll)
 
+def to_pointer(numpy_array): 
+    """
+    Converts the supplied numpy_array (assumed to be the usual 64-bit float)
+    to a pointer, allowing it ot be "connected" to the C-code engine.
+    
+    SUPER IMPORTANT
+    ---------------
+    Make sure you assign your array to a variable name prior to calling this.
+    If you do not keep a handle on the numpy array, garbage collection can
+    delete it while the simulation is running!
+    
+    Parameters
+    ----------
+    numpy_array
+        1D ndarray of 64-bit floats.
+        
+    Returns
+    -------
+    C-pointer to the first element.
+    
+    Examples
+    --------
+    my_solver.Bxs = to_pointer(my_Bxs).
+    
+    """
+    return numpy_array.ctypes.data_as(_c.POINTER(_c.c_double))
+
+
+
 class _domain(_c.Structure):
     """
     Structure for sending all the simulation parameters to the c library.
@@ -21,70 +50,105 @@ class _domain(_c.Structure):
     _fields_ = [
         
         # Magnitude of the gyromagnetic ratio [radians / (sec T)]
-        ('gamma', _c.c_double), ("gammas", _c.POINTER(_c.c_double)),
+        ('gamma', _c.c_double), ("_gammas", _c.POINTER(_c.c_double)),
         
         #Gilbert damping parameter [unitless]
-        ('M', _c.c_double), ("Ms", _c.POINTER(_c.c_double)),
+        ('M', _c.c_double), ("_Ms", _c.POINTER(_c.c_double)),
         
         # Gilbert damping
-        ('alpha', _c.c_double), ('alphas', _c.POINTER(_c.c_double)),
+        ('alpha', _c.c_double), ('_alphas', _c.POINTER(_c.c_double)),
         
         # Exchange-like field strength [T], applied in the direction of the other domain's unit vector
-        ('X', _c.c_double), ("Xs", _c.POINTER(_c.c_double)),
+        ('X', _c.c_double), ('_Xs', _c.POINTER(_c.c_double)),
         
         # Spin transfer torque (rate) parallel to other domain [rad / s]
-        ('s', _c.c_double), ("ss", _c.POINTER(_c.c_double)),
+        ('s', _c.c_double), ('_ss', _c.POINTER(_c.c_double)),
         
         # Other torque (rate) unrelated to either domain [rad / s]
-        ('tx', _c.c_double), ("txs", _c.POINTER(_c.c_double)),
-        ('ty', _c.c_double), ("tys", _c.POINTER(_c.c_double)),
-        ('tz', _c.c_double), ("tzs", _c.POINTER(_c.c_double)),
+        ('tx', _c.c_double), ('_txs', _c.POINTER(_c.c_double)),
+        ('ty', _c.c_double), ('_tys', _c.POINTER(_c.c_double)),
+        ('tz', _c.c_double), ('_tzs', _c.POINTER(_c.c_double)),
         
         # Externally applied field
-        ('Bx', _c.c_double), ("Bxs", _c.POINTER(_c.c_double)),
-        ('By', _c.c_double), ("Bys", _c.POINTER(_c.c_double)),
-        ('Bz', _c.c_double), ("Bzs", _c.POINTER(_c.c_double)),
+        ('Bx', _c.c_double), ('_Bxs', _c.POINTER(_c.c_double)),
+        ('By', _c.c_double), ('_Bys', _c.POINTER(_c.c_double)),
+        ('Bz', _c.c_double), ('_Bzs', _c.POINTER(_c.c_double)),
         
         # Anisotropy tensor elements [unitless], defined such that Nxx+Nyy+Nzz=1 for an aligned ellipsoid
-        ('Nxx', _c.c_double), ("Nxxs", _c.POINTER(_c.c_double)),
-        ('Nxy', _c.c_double), ("Nxys", _c.POINTER(_c.c_double)),
-        ('Nxz', _c.c_double), ("Nxzs", _c.POINTER(_c.c_double)),
+        ('Nxx', _c.c_double), ('_Nxxs', _c.POINTER(_c.c_double)),
+        ('Nxy', _c.c_double), ('_Nxys', _c.POINTER(_c.c_double)),
+        ('Nxz', _c.c_double), ('_Nxzs', _c.POINTER(_c.c_double)),
+    
+        ('Nyx', _c.c_double), ('_Nyxs', _c.POINTER(_c.c_double)),
+        ('Nyy', _c.c_double), ('_Nyys', _c.POINTER(_c.c_double)),
+        ('Nyz', _c.c_double), ('_Nyzs', _c.POINTER(_c.c_double)),
         
-        ('Nyx', _c.c_double), ("Nyxs", _c.POINTER(_c.c_double)),
-        ('Nyy', _c.c_double), ("Nyys", _c.POINTER(_c.c_double)),
-        ('Nyz', _c.c_double), ("Nyzs", _c.POINTER(_c.c_double)),
-        
-        ('Nzx', _c.c_double), ("Nzxs", _c.POINTER(_c.c_double)),
-        ('Nzy', _c.c_double), ("Nzys", _c.POINTER(_c.c_double)),
-        ('Nzz', _c.c_double), ("Nzzs", _c.POINTER(_c.c_double)),
-        
+        ('Nzx', _c.c_double), ('_Nzxs', _c.POINTER(_c.c_double)),
+        ('Nzy', _c.c_double), ('_Nzys', _c.POINTER(_c.c_double)),
+        ('Nzz', _c.c_double), ('_Nzzs', _c.POINTER(_c.c_double)),
+    
         # Dipole tensor [unitless], representing the fraction of the other layer's saturation magnetization
-        ('Dxx', _c.c_double), ("Dxxs", _c.POINTER(_c.c_double)),
-        ('Dxy', _c.c_double), ("Dxys", _c.POINTER(_c.c_double)),
-        ('Dxz', _c.c_double), ("Dxzs", _c.POINTER(_c.c_double)),
+        ('Dxx', _c.c_double), ('_Dxxs', _c.POINTER(_c.c_double)),
+        ('Dxy', _c.c_double), ('_Dxys', _c.POINTER(_c.c_double)),
+        ('Dxz', _c.c_double), ('_Dxzs', _c.POINTER(_c.c_double)),
         
-        ('Dyx', _c.c_double), ("Dyxs", _c.POINTER(_c.c_double)),
-        ('Dyy', _c.c_double), ("Dyys", _c.POINTER(_c.c_double)),
-        ('Dyz', _c.c_double), ("Dyzs", _c.POINTER(_c.c_double)),
+        ('Dyx', _c.c_double), ('_Dyxs', _c.POINTER(_c.c_double)),
+        ('Dyy', _c.c_double), ('_Dyys', _c.POINTER(_c.c_double)),
+        ('Dyz', _c.c_double), ('_Dyzs', _c.POINTER(_c.c_double)),
         
-        ('Dzx', _c.c_double), ("Dzxs", _c.POINTER(_c.c_double)),
-        ('Dzy', _c.c_double), ("Dzys", _c.POINTER(_c.c_double)),
-        ('Dzz', _c.c_double), ("Dzzs", _c.POINTER(_c.c_double)),
+        ('Dzx', _c.c_double), ('_Dzxs', _c.POINTER(_c.c_double)),
+        ('Dzy', _c.c_double), ('_Dzys', _c.POINTER(_c.c_double)),
+        ('Dzz', _c.c_double), ('_Dzzs', _c.POINTER(_c.c_double)),
         
         # Solver mode: 0=disabled, 1=LLG
         ('mode', _c.c_int),
         
         # Solution arrays
-        ("x", _c.POINTER(_c.c_double)),
-        ("y", _c.POINTER(_c.c_double)),
-        ("z", _c.POINTER(_c.c_double)),
+        ('_x', _c.POINTER(_c.c_double)),
+        ('_y', _c.POINTER(_c.c_double)),
+        ('_z', _c.POINTER(_c.c_double)),
         
     ] # End of _data structure
     
+    def set(self, **kwargs): 
+        """
+        Sets any number of parameters for this domain.
+
+        Parameters
+        ----------
+        **kwargs : keyword will be set by evaluating self.keyword = value. In 
+        the case of an array, it will connect the appropriate (64-bit float) 
+        pointer. This will also save a "local" copy of the supplied value, such
+        that garbage collection doesn't automatically delete it.
+        
+        Example
+        -------
+        my_solver.a.set(Bys=numpy.linspace(0,5,my_solver.steps), gamma=27)
+
+        Returns
+        -------
+        self
+
+        """
+        
+        for k in kwargs:
+
+            # Store the "local" copy, to make sure we keep it from getting
+            # deleted by the garbage collection.
+            exec('self.'+k+"=v", dict(self=self,v=kwargs[k]))
+            
+            # If it's an array, convert it before setting
+            if type(kwargs[k])==_n.ndarray:
+                exec('self._'+k+"=v", dict(self=self,v=to_pointer(kwargs[k])))
+            
+        return self
+        
+
+
 
 class solver():
     """
-    Class for interfacing with the macrospin simulation c-library.
+    Class for interfacing with the compiled C-code engine.
     """
     
     def __init__(self, dt=0.005, steps=1e5):
@@ -95,7 +159,7 @@ class solver():
         
         # Initial conditions
         self.ax0   = 1.0
-        self.ay0   = 0.0
+        self.ay0   = 2.0
         self.az0   = 0.0
         self.bx0   = 1.0
         self.by0   = 0.0
@@ -107,40 +171,39 @@ class solver():
         
         # Null all the array pointers, just to be safe 
         # (different platforms, Python versions, etc...)
-        self.a.gammas = self.b.gammas = None
-        self.a.Ms     = self.b.Ms     = None
-        self.a.alphas = self.b.alphas = None
-        self.a.Xs     = self.b.Xs     = None
-        self.a.ss     = self.b.ss     = None
+        self.a._gammas = self.b._gammas = None
+        self.a._Ms     = self.b._Ms     = None
+        self.a._alphas = self.b._alphas = None
+        self.a._Xs     = self.b._Xs     = None
+        self.a._ss     = self.b._ss     = None
         
-        self.a.Bxs    = self.b.Bxs    = None
-        self.a.Bys    = self.b.Bys    = None
-        self.a.Bzs    = self.b.Bzs    = None
+        self.a._Bxs    = self.b._Bxs    = None
+        self.a._Bys    = self.b._Bys    = None
+        self.a._Bzs    = self.b._Bzs    = None
         
-        self.a.txs    = self.b.txs    = None
-        self.a.tys    = self.b.tys    = None
-        self.a.tzs    = self.b.tzs    = None
+        self.a._txs    = self.b._txs    = None
+        self.a._tys    = self.b._tys    = None
+        self.a._tzs    = self.b._tzs    = None
         
-        self.a.Nxxs   = self.b.Nxxs   = None
-        self.a.Nxys   = self.b.Nxys   = None
-        self.a.Nxzs   = self.b.Nxzs   = None
-        self.a.Nyxs   = self.b.Nyxs   = None
-        self.a.Nyys   = self.b.Nyys   = None
-        self.a.Nyzs   = self.b.Nyzs   = None
-        self.a.Nzxs   = self.b.Nzxs   = None
-        self.a.Nzys   = self.b.Nzys   = None
-        self.a.Nzzs   = self.b.Nzzs   = None
+        self.a._Nxxs   = self.b._Nxxs   = None
+        self.a._Nxys   = self.b._Nxys   = None
+        self.a._Nxzs   = self.b._Nxzs   = None
+        self.a._Nyxs   = self.b._Nyxs   = None
+        self.a._Nyys   = self.b._Nyys   = None
+        self.a._Nyzs   = self.b._Nyzs   = None
+        self.a._Nzxs   = self.b._Nzxs   = None
+        self.a._Nzys   = self.b._Nzys   = None
+        self.a._Nzzs   = self.b._Nzzs   = None
         
-        self.a.Dxxs   = self.b.Dxxs   = None
-        self.a.Dxys   = self.b.Dxys   = None
-        self.a.Dxzs   = self.b.Dxzs   = None
-        self.a.Dyxs   = self.b.Dyxs   = None
-        self.a.Dyys   = self.b.Dyys   = None
-        self.a.Dyzs   = self.b.Dyzs   = None
-        self.a.Dzxs   = self.b.Dzxs   = None
-        self.a.Dzys   = self.b.Dzys   = None
-        self.a.Dzzs   = self.b.Dzzs   = None
-        
+        self.a._Dxxs   = self.b._Dxxs   = None
+        self.a._Dxys   = self.b._Dxys   = None
+        self.a._Dxzs   = self.b._Dxzs   = None
+        self.a._Dyxs   = self.b._Dyxs   = None
+        self.a._Dyys   = self.b._Dyys   = None
+        self.a._Dyzs   = self.b._Dyzs   = None
+        self.a._Dzxs   = self.b._Dzxs   = None
+        self.a._Dzys   = self.b._Dzys   = None
+        self.a._Dzzs   = self.b._Dzzs   = None
         
         # By default, enable the two domains.
         self.a.mode=1
@@ -169,12 +232,12 @@ class solver():
         self.bz = _n.zeros(self.steps); self.bz[0] = self.bz0
         
         # Provide the c-code with access to the numpy array data
-        self.a.x = _n.ctypeslib.as_ctypes(self.ax)
-        self.a.y = _n.ctypeslib.as_ctypes(self.ay)
-        self.a.z = _n.ctypeslib.as_ctypes(self.az)
-        self.b.x = _n.ctypeslib.as_ctypes(self.bx)
-        self.b.y = _n.ctypeslib.as_ctypes(self.by)
-        self.b.z = _n.ctypeslib.as_ctypes(self.bz)
+        self.a._x = to_pointer(self.ax)
+        self.a._y = self.ay.ctypes.data_as(_c.POINTER(_c.c_double))
+        self.a._z = self.az.ctypes.data_as(_c.POINTER(_c.c_double))
+        self.b._x = self.bx.ctypes.data_as(_c.POINTER(_c.c_double))
+        self.b._y = self.by.ctypes.data_as(_c.POINTER(_c.c_double))
+        self.b._z = self.bz.ctypes.data_as(_c.POINTER(_c.c_double))
         
         # Solve it.
         _engine.solve_heun.restype = None
@@ -185,20 +248,27 @@ class solver():
         
         return self
 
+    
 
 
-
-m = solver()
-m.steps=1000
-
-m.a.gamma=2*_n.pi; m.a.By=1.0
-m.b.gamma=2*_n.pi; m.b.By=1.0
-
-m.run()
-
-import spinmob as sm
-sm.plot.xy.data(m.dt, [m.ax,m.ay,m.az,m.bx,m.by,m.bz],
-                label=['ax','ay','az','bx','by','bz'])
+if __name__ == '__main__':
+    
+    # Create a solver instance
+    m = solver(dt=0.005, steps=1e3)
+    
+    # Set up the physical parameters
+    m.a.set(gamma=2*_n.pi, By=1.0, Bys=_n.linspace(0,1,m.steps))
+    m.b.set(gamma=2*_n.pi, By=1.0)
+    
+    # Run it
+    m.run()
+    
+    # Plot.
+    n1=0
+    import spinmob as sm
+    sm.plot.xy.data(m.dt, [m.ax[n1:],m.ay[n1:],m.az[n1:],m.bx[n1:],m.by[n1:],m.bz[n1:]],
+                    label=['ax','ay','az','bx','by','bz'], 
+                    xlabel='Time (s)', ylabel='Projection (unitless)')
     
     
     
